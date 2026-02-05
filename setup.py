3ecfa690b4f2bcff28499cfc90b2d578a7ca4f5b
Raghuraamm <Raghusony2005@gmail.com>
2026-02-06 02:13:24 +0530

    [bug] fixed same name dist glitch

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+try:
+    from setuptools.dist import Distribution
+    class BinaryDistribution(Distribution):
+        """Force the wheel to be platform-specific."""
+        def has_ext_modules(self):
+            return True
+        def is_pure(self):
+            return False
+except ImportError:
+    BinaryDistribution = None
+
+setup(
+    distclass=BinaryDistribution,
+)
